19a9e001c074bb1edeaa4209025ec2d0641022b0
Kenneth Carlton <kencarlton55@gmail.com>
2023-02-06 22:04:45 -0600

    Save most recent changes

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Intended Audience :: Manufacturing',
         'Intended Audience :: End Users/Desktop',
         'Operating System :: OS Independent',],
-    install_requires = ['bomcheck>=1.8', 'pyside2>=5.15', 'qtawesome>=1.0'],
+    install_requires = ['bomcheck>=1.8.5', 'PyQt5>=5.15', 'qtawesome>=1.0'],
     url='https://github.com/kcarlton55/bomcheckgui',
     author='Kenneth Edward Carlton',
     author_email='kencarlton55@gmail.com',
